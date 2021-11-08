5febf964e59dce3951041a6749bde690ec655544
Eyal Vosk <Eyal.Vosk@au10tix.com>
2021-11-08 08:03:37 +0200

    Test

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
+#
+# app = Flask(__name__)
 
-app = Flask(__name__)
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'title1',
-        'description': u'artifact1',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'title2',
-        'description': u'artifact2',
-        'done': False
-    }
-]
-
-
-def get_tasks():
-    return jsonify({'tasks': tasks})
+# tasks = [
+#     {
+#         'id': 1,
+#         'title': u'title1',
+#         'description': u'artifact1',
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'title': u'title2',
+#         'description': u'artifact2',
+#         'done': False
+#     }
+# ]
+#
+#
+# def get_tasks():
+#     return jsonify({'tasks': tasks})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    print("Hello")
