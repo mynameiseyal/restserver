75f46150c91ec2ce29a58ed66b3e0efab6854b92
Eyal.Vosk <mynameiseyal@gmail.com>
2021-11-10 08:00:12 +0200

    Added home route

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>You've reached home</h1><p>This is a rest API server for Au10tix</p>"
+
+
 @app.route('/companies', methods=['GET'])
 def get_companies():
   return json.dumps(companies)
+
 
 if __name__ == '__main__':
     app.run()
