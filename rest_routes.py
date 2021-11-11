e2c268bd4e35992b6df077d83154666fad3e04aa
Eyal Vosk <Eyal.Vosk@au10tix.com>
2021-11-11 08:25:12 +0200

    Returned If, added JSON check

--- rest_routes.py
+++ rest_routes.py
@@ -3,12 +3,16 @@ from json_replies import responses
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
+    """
+    :return: Home message
+    """
     return "<h1>You've reached home</h1><p>This is a rest API server for Au10tix</p>"
 
 
-@app.route('/test/all', methods=['GET'])
+@app.route('/api/all', methods=['GET'])
 def get_responses():
     """
     :return: all responses
@@ -16,21 +20,16 @@ def get_responses():
     return jsonify(responses)
 
 
-@app.route('/test/responses', methods=['GET'])
+@app.route('/api/responses', methods=['GET'])
 def api_id():
     """
     Checks if the args contains an id of the response
     :return: response according to id or error message
     """
-    try:
+    if 'id' in request.args:
         req_id = int(request.args['id'])
-    except None as e:
+    else:
         return "Error, check your code"
-
-    # if 'id' in request.args:
-    #     req_id = int(request.args['id'])
-    # else:
-    #     return "Error, check your code"
 
     results = []
     for resp in responses:
@@ -38,3 +37,9 @@ def api_id():
             results.append(resp)
 
     return jsonify(results)
+
+
+@app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
+def add_message(uuid):
+    content = request.get_json()
+    return content
