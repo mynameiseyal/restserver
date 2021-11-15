d9d069fa37c3771a5f13382791f1ba146ad88546
Eyal Vosk <Eyal.Vosk@au10tix.com>
2021-11-15 12:01:00 +0200

    Added expected JSON verification

--- rest_routes.py
+++ rest_routes.py
@@ -52,7 +52,7 @@ def process_responses():
                     "serial": 3}
     :return: Correct if right, Incorrect if not
     """
-    content = request.get_json()
+    content = request.json()
 
     parsed = json.loads(content, object_hook=lambda d: SimpleNamespace(**d))
     print(content)
